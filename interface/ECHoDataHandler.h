eb282d1ffffd8dd2c3b9d8f8e8295cb5a288d6d4
Federica Mantegazzini <federica.mantegazzini@cern.ch>
2016-07-12 14:49:00 +0200

    Create ECHoDataHandler.h

---
+++ interface/ECHoDataHandler.h
@@ -0,0 +1,13 @@
+/*****************************************************************************
+Class ECHoDataHandler:
+Methods:
+TreeMaker: fill a TTree from an ASCII file with the useful variables
+TreeReader: read the TTree created with TreeMaker (or other methods) and 
+            save some plots in a TTree
+PlotsDrawer: read the TTree created with TreeReader, draw the plots and
+             save them as images
+TreeUpdater: continue to fill an existing TTree
+-----------------------------------------------------------------------------
+ECHo Collaboration
+Federica Mantegazzini
+*****************************************************************************/
